bb95af67d262abad6ce929f9ebd36e11f45b6480
TheRealVira <therealvira@gmail.com>
2018-11-21 11:02:52 +0100

    Added ocelot base gateway and reorganized project.

---
+++ Services/ApiGw-Base/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace ApiGw_Base
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            var builder = WebHost.CreateDefaultBuilder(args);
+
+            builder.ConfigureServices(s => s.AddSingleton(builder))
+                .ConfigureAppConfiguration(
+                    ic => ic.AddJsonFile(Path.Combine("configuration",
+                        "configuration.json")))
+                .UseStartup<Startup>();
+            return builder;
+        }
+    }
+}
